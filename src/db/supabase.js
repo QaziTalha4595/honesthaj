1d63a07df7d93ae703fa8efe0518b00cbdbd0fe9
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2024-09-02 21:50:56 +0530

    maybe this'll do it

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://glqpqmxowaoicrekkhlv.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdscXBxbXhvd2FvaWNyZWtraGx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyOTEyMTIsImV4cCI6MjA0MDg2NzIxMn0.bvhDmysmp6qw2haM-BK8YuGw48Hw-Z0aLmE2JMvUBfM";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
         
