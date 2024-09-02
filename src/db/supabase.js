c7ca97cb625801424c364cae191f1f11a9c3c65c
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2024-09-02 21:43:20 +0530

    Update supabase.js

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseUrl = "https://glqpqmxowaoicrekkhlv.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdscXBxbXhvd2FvaWNyZWtraGx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyOTEyMTIsImV4cCI6MjA0MDg2NzIxMn0.bvhDmysmp6qw2haM-BK8YuGw48Hw-Z0aLmE2JMvUBfM";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
         
